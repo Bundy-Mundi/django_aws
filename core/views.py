d3778dfe85edb01f1fa6bda9ae85232474020508
Ben <bennie.ben095@gmail.com>
2019-12-27 07:13:04 +0900

    Set django.config

--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from users.models import User
 import requests
 
 class HomeView(View):
 
     def get(self, request):
-
+        users = User.objects.all()
         name = "Home"
+
         requests.post("https://bennie.netlify.com/#/", data={"message":"Hello"}, json={"message":"Hello"})
-        return render(request, "home.html", {"name":name})
+
+        return render(request, "home.html", {"name":name, "users":users})
 
